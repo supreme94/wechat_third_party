c113e649bd81c0f95bd3216c5609cc5909dee1ef
liangpeng <183407401@qq.com>
2017-07-22 16:39:08 +0800

    增加基础类

--- src/main/java/com/github/supreme94/wechat/core/WechatController.java
+++ src/main/java/com/github/supreme94/wechat/core/WechatController.java
@@ -1,16 +1,27 @@
 package com.github.supreme94.wechat.core;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.github.supreme94.wechat.core.config.WechatProperties;
+import com.github.supreme94.wechat.pojo.WxMpXmlMessage;
+import com.github.supreme94.wechat.service.ValidatorService;
 
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
 public class WechatController {
-
+	
+	@Autowired
+	private ValidatorService validatorService;
+	
+	@Autowired
+	private WechatProperties wechatProperties;
+	
 	@PostMapping("/ticket")
 	public String recevieEventFromWechat(@RequestBody String requestBody,
 										@RequestParam("signature") String signature,
@@ -23,6 +34,22 @@ public class WechatController {
                         + " timestamp=[{}], nonce=[{}], requestBody=[\n{}\n] ",
                 signature, encType, msgSignature, timestamp, nonce, requestBody);
 		
+        if (!this.validatorService.checkSignature(timestamp, nonce, signature)) {
+            throw new IllegalArgumentException("非法请求，可能属于伪造的请求！");
+        }
+        
+        if (encType == null) {
+            // 明文传输的消息
+            WxMpXmlMessage inMessage = WxMpXmlMessage.fromXml(requestBody);
+            System.out.println(inMessage);
+        } else if ("aes".equals(encType)) {
+            // aes加密的消息
+            Object inMessage = WxMpXmlMessage.fromEncryptedXml(
+                    requestBody, this.wechatProperties, timestamp,
+                    nonce, msgSignature);
+            log.debug("\n消息解密后内容为：\n{} ", inMessage.toString());
+        }
+        
 		return "success";
 	}
 }
