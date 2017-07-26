cf8e52af16696833ba59afdb6100e60786af9d32
liangpeng <183407401@qq.com>
2017-07-26 18:32:49 +0800

    remove all

--- src/main/java/com/github/supreme94/wechat/util/xml/XStreamMediaIdConverter.java
+++
@@ -1,8 +0,0 @@
-package com.github.supreme94.wechat.util.xml;
-
-public class XStreamMediaIdConverter extends XStreamCDataConverter {
-  @Override
-  public String toString(Object obj) {
-    return "<MediaId>" + super.toString(obj) + "</MediaId>";
-  }
-}
